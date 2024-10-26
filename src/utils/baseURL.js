4c4ba6fe77bc42c53ea571c084cf2d1c54ecc01e
Nathaniel Getachew <nathigechmaranata@gmail.com>
2024-10-26 23:56:29 +0300

    backend deployed

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,4 @@
-const   BASE_URL  = "http://localhost:9080/api/v1"
+// const   BASE_URL  = "http://localhost:9080/api/v1"
+const BASE_URL = "https://mern-blog-project-backend.onrender.com/api/v1"
  
 export default BASE_URL;
